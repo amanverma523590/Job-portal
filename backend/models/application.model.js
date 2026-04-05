686178a16aeb81e91ab1fc7129006f6e2fca19fc
Aman Verma <aman97104@gmail.com>
2026-04-05 12:24:44 +0530

    routes of application

--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -18,4 +18,4 @@ const applicationSchema = new mongoose.Schema({
     },
 },{timestamps:true})
 
-export const Application = mongoose.model('Applications',applicationSchema);
+export const Application = mongoose.model('Application',applicationSchema);
