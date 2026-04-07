2a603b563e723dfb05ebb0d815b921e896d463e0
Aman Verma <aman97104@gmail.com>
2026-04-07 14:54:23 +0530

    popover

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,28 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Avatar, AvatarImage } from "../ui/avatar";
 
 const Navbar = () => {
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16 px-8">
-         <div>
+        <div>
           <h1 className="text-2xl font-bold">
             Job<span className="text-[#f83002]">Portal</span>
           </h1>
-         </div>
-         <div>
+        </div>
+        <div>
           <ul className="flex font-medium gap-5 items-center">
             <li>Home</li>
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
-         </div>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent>
+              <h1>hellow</h1>
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Navbar;
-
-
-//ok
