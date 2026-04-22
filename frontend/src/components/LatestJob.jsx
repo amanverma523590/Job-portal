164803bf0388f87384c7d6d2b12f6ad98852d334
Aman Verma <aman97104@gmail.com>
2026-04-22 16:20:25 +0530

    added

--- frontend/src/components/LatestJob.jsx
+++ frontend/src/components/LatestJob.jsx
@@ -1,14 +1,18 @@
+import { useSelector } from "react-redux"
 import LatestJobCard from "./LatestJobCard"
 
 const randomJob = [1,2,3,4,5,6,7,8]
 
 const LatestJob = () => {
+
+  const {allJobs} = useSelector(store=>store.job)
+
   return (
     <div className="max-w-5xl mx-auto my-20">
       <h1 className="text-4xl font-bold"> <span className="text-[#6A38C2]">Latest & Top</span> Job Openings</h1>
       <div className="grid grid-cols-3 gap-4 my-5">
         {
-            randomJob.slice(0,6).map( (item,index)=><LatestJobCard/> )
+            allJobs.length  <= 0 ? <span>No job avilable </span> : allJobs.slice(0,6).map( (job)=><LatestJobCard  key={job._id} job={job} /> )
         }
       </div>
         
--- frontend/src/components/LatestJobCard.jsx
+++ frontend/src/components/LatestJobCard.jsx
@@ -1,14 +1,14 @@
 import { Badge} from './ui/badge'
 
-const LatestJobCard = () => {
+const LatestJobCard = (job) => {
   return (
     <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
       <div>
-        <h1 className='font-medium text-lg'>Company Name</h1>
+        <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
         <p className='text-sm text-gray-500'>India</p>
       </div>
       <div>
-        <h1 className='font-bold text-lg my-2'>Job Title</h1>
+        <h1 className='font-bold text-lg my-2'>{}</h1>
         <p className='text-sm text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid?</p>
       </div>
       <div className='flex items-center gap-2 mt-4'>
