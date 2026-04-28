f000ae130b2228f8c9e26528ef6891319bfabff8
Aman Verma <aman97104@gmail.com>
2026-04-28 13:15:03 +0530

    posting job page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Companies from "./components/admin/Companies";
 import CompanyCreate from "./components/admin/CompanyCreate";
 import CompanySetup from "./components/admin/CompanySetup";
 import AdminJobs from "./components/admin/AdminJobs";
+import PostJob from "./components/admin/PostJob";
 
 
 const appRouter = createBrowserRouter([
@@ -58,7 +59,11 @@ const appRouter = createBrowserRouter([
   {
     path:"/admin/jobs",
     element:<AdminJobs/>
-  }
+  },
+  {
+    path:"/admin/jobs/create",
+    element:<PostJob/>
+  },
 
 ]);
 
